6bde2822ef19205f141b9eae3d83e7de280ff3ce
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-05-25 11:37:42 +0530

    created route

--- app/Component/HomePage/HomePage.js
+++ app/Component/HomePage/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
   return (
     <>
       {" "}
-      <div className="container mx-auto max-w-6xl py-10">
+      <div className="container mx-auto max-w-6xl py-2 md:py-10">
         <Swiper className="mySwiper">
           {img.map((el, i) => {
             return (
@@ -34,6 +34,96 @@ const HomePage = () => {
           })}
         </Swiper>
       </div>
+      <div className="text-center text-lg font-medium p-5">
+        <h1>TOPTASTIC DEALS</h1>
+      </div>
+      <section className="container mx-auto max-w-6xl">
+        <div className="grid gap-10 md:grid-cols-4 mx-auto place-content-center ">
+          <div class="bg-white shadow-md rounded-lg max-w-sm w-full">
+            <img
+              src="https://topspizza.co.uk/storage/SEPMMLZptaCkuMv8xtHyyrJaN2NOaMCoBo4c7t53.jpg"
+              alt="Card Image"
+              class="rounded-t-lg w-full  object-cover"
+            />
+            <div class="p-4">
+              <h2 class="text-xl font-semibold mb-4">Hungry Man</h2>
+              <div class="relative">
+                <select class="w-full border border-gray-300 rounded-lg p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                  <option value="option1">Large 1$</option>
+                  <option value="option2">Large 2$</option>
+                  <option value="option3">Large 3$</option>
+                </select>
+                <button class="absolute inset-y-0 right-0 flex items-center bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600">
+                  Go
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white shadow-md rounded-lg max-w-sm w-full">
+            <img
+              src="https://topspizza.co.uk/storage/SEPMMLZptaCkuMv8xtHyyrJaN2NOaMCoBo4c7t53.jpg"
+              alt="Card Image"
+              class="rounded-t-lg w-full  object-cover"
+            />
+            <div class="p-4">
+              <h2 class="text-xl font-semibold mb-4">Hungry Man</h2>
+              <div class="relative">
+                <select class="w-full border border-gray-300 rounded-lg p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                  <option value="option1">Large 1$</option>
+                  <option value="option2">Large 2$</option>
+                  <option value="option3">Large 3$</option>
+                </select>
+                <button class="absolute inset-y-0 right-0 flex items-center bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600">
+                  Go
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white shadow-md rounded-lg max-w-sm w-full">
+            <img
+              src="https://topspizza.co.uk/storage/SEPMMLZptaCkuMv8xtHyyrJaN2NOaMCoBo4c7t53.jpg"
+              alt="Card Image"
+              class="rounded-t-lg w-full  object-cover"
+            />
+            <div class="p-4">
+              <h2 class="text-xl font-semibold mb-4">Hungry Man</h2>
+              <div class="relative">
+                <select class="w-full border border-gray-300 rounded-lg p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                  <option value="option1">Large 1$</option>
+                  <option value="option2">Large 2$</option>
+                  <option value="option3">Large 3$</option>
+                </select>
+                <button class="absolute inset-y-0 right-0 flex items-center bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600">
+                  Go
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white shadow-md rounded-lg max-w-sm w-full">
+            <img
+              src="https://topspizza.co.uk/storage/SEPMMLZptaCkuMv8xtHyyrJaN2NOaMCoBo4c7t53.jpg"
+              alt="Card Image"
+              class="rounded-t-lg w-full  object-cover"
+            />
+            <div class="p-4">
+              <h2 class="text-xl font-semibold mb-4">Hungry Man</h2>
+              <div class="relative">
+                <select class="w-full border border-gray-300 rounded-lg p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                  <option value="option1">Large 1$</option>
+                  <option value="option2">Large 2$</option>
+                  <option value="option3">Large 3$</option>
+                </select>
+                <button class="absolute inset-y-0 right-0 flex items-center bg-blue-500 text-white p-2 rounded-r-lg hover:bg-blue-600">
+                  Go
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
