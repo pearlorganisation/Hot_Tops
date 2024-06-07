e93edd1665f3d2b248340743222c2a25dfb189f3
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-06-07 12:14:30 +0530

    new update

--- client/app/(routes)/menu/product/[productName]/page.jsx
+++ client/app/(routes)/menu/product/[productName]/page.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const product = () => {
-  return <div>product</div>;
+  return (
+    <>
+      <div className="container grid grid-cols-[60%_auto]">
+        <div>
+          <h1>HAWAIIAN</h1>
+          <p>Tops mozzarella cheese, Special tomato sauce, Pineapple & Ham</p>
+          <button className="w-full bg-[#28A745]">Save</button>
+        </div>
+        <div className="">
+          <img src="" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default product;
