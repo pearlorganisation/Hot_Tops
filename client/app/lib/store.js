48c859ae4e70edef7344544d58ccf03f96a9de11
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-06-17 16:11:37 +0530

    minor changes

--- client/app/lib/store.js
+++ client/app/lib/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import selectedStoreReducer from "./features/selectedStore/selectedStoreSlice";
 import cartReducer from "./features/cartSlice/cartSlice.js";
-import pizzaReducer from "./features/slices/pizzaSlice/pizzeSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
       selectedStore: selectedStoreReducer,
       cart: cartReducer,
-      pizza: pizzaReducer,
     },
   });
 };
