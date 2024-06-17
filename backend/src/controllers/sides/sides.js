d8d58aa8ea627b6e360251285e4e8965129af467
Saksham <saksham@pearlorganisation.com>
2024-06-17 14:42:52 +0530

    create sides

---
+++ backend/src/controllers/sides/sides.js
@@ -0,0 +1,19 @@
+import sides from "../../models/sides/sides.js";
+import { upload } from "../../configs/cloudinary.js";
+import { asyncErrorHandler } from "../../utils/errors/asyncErrorHandler.js";
+import { CustomError } from "../../utils/errors/customError.js";
+
+export const newSides = asyncErrorHandler(async (req, res, next) => {
+  const savedData = new sides({
+    ...req?.body,
+    banner: req?.file?.path,
+    priceSection: JSON.parse(req?.body?.priceSection),
+  });
+  await savedData.save();
+  res.status(201).json({ status: true, message: "Created successfully!!" });
+});
+
+export const getAllSides = asyncErrorHandler(async (req, res, next) => {
+  const data = await sides.find();
+  res.status(200).json({ status: true, data });
+});
