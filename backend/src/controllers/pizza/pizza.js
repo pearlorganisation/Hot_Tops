513cec97dcd7d4b6400507536844f318cb01e428
avnishPearlorganisation <161312155+avnishPearlorganisation@users.noreply.github.com>
2024-06-20 12:15:59 +0530

    Update pizza.js

--- backend/src/controllers/pizza/pizza.js
+++ backend/src/controllers/pizza/pizza.js
@@ -3,27 +3,17 @@ import { CustomError } from "../../utils/errors/customError.js";
 import pizza from "../../models/pizza/pizza.js";
 
 export const createNewPizza = asyncErrorHandler(async (req, res, next) => {
-  const data = JSON.parse(req?.body?.data);
-  console.log(data);
-  // const { small, medium, large, supersize, ...rest } = data;
-  const keys = Object.keys(data);
+const  {priceSection}= req?.body
 
-  const priceSection = [
-    { size: "small", price: data?.small },
-    { size: "large", price: data?.large },
-    { size: "medium", price: data?.medium },
-    { size: "supersize", price: data?.supersize },
-  ];
-  console.log(priceSection);
-  const newPizza = await new pizza({
-    ...data,
+  const newPizza = new pizza({
+    ...req?.body,
     banner: req?.file?.path,
-    priceSection: priceSection,
+    priceSection: JSON.parse(priceSection),
   });
   await newPizza.save();
   res
     .status(200)
-    .json({ status: true, message: "Pizza created csuccessfully!!" ,newPizza});
+    .json({ status: true, message: "Pizza created successfully!!" ,newPizza});
 });
 
 export const getAllPizza = asyncErrorHandler(async (req, res, next) => {
