b8286daed7bf40a72a918af29e7c649368ccee22
Gaurav Rajput <gauravrajput@pearlorganisation.com>
2024-06-21 17:23:58 +0530

    set the price in header to 2 decimal value

--- client/app/_components/Modals/AddedToCartModel.jsx
+++ client/app/_components/Modals/AddedToCartModel.jsx
@@ -35,23 +35,16 @@ const AddedToCartModel = ({ isAddClicked, setIsAddClicked }) => {
               </svg>
               <span class="sr-only">Close modal</span>
             </button>
-            <div class="p-4 md:p-5 text-center">
+            <div class="p-4 md:p-5 flex justify-center flex-col items-center">
               <svg
-                class="mx-auto mb-4 text-red-600 w-12 h-12 dark:text-gray-200"
-                aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
+                width="100"
+                height="100"
+                viewBox="0 0 24 24"
               >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
+                <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z" />
               </svg>
-              <h3 class="mb-5 text-lg font-normal text-red-600 dark:text-gray-400">
+              <h3 class="mb-5 text-lg font-normal text-black dark:text-gray-400">
                 Added to Cart!
               </h3>
             </div>
