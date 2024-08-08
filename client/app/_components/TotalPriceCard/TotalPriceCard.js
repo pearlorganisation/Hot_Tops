0bcef16c466f59cb89a9199bc2cca9de6a059702
Avnish Negi <avnish@pearlorganisation.com>
2024-08-08 12:26:26 +0530

    pizza

--- client/app/_components/TotalPriceCard/TotalPriceCard.js
+++ client/app/_components/TotalPriceCard/TotalPriceCard.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { createPortal } from "react-dom";
+// import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 
 const TotalPriceCard = () => {
@@ -8,18 +8,18 @@ const TotalPriceCard = () => {
     (state) => state.cart
   );
 
-  const [mount, setMount] = useState(false);
-  useEffect(() => {
-    setMount(true);
-    return () => {
-      setMount(false);
-    };
-  }, []);
+  // const [mount, setMount] = useState(false);
+  // useEffect(() => {
+  //   setMount(true);
+  //   return () => {
+  //     setMount(false);
+  //   };
+  // }, []);
 
   return (
-    <>
-      {mount &&
-        createPortal(
+    // <>
+    //   {mount &&
+    //     createPortal(
           <div className="fixed bottom-5 rounded-md right-5 bg-red-600 p-6 text-white font-semibold text-center">
             <div> Price : {allToppings?.price}</div>
             <div>
@@ -27,10 +27,11 @@ const TotalPriceCard = () => {
               {Math.max(0, (allToppings?.extraPrice - defaultPrice).toFixed(2))}
             </div>
             <div>Total Price : {allToppings?.totalPrice}</div>
-          </div>,
-          document.body
-        )}
-    </>
+          </div>
+    //       ,
+    //       document.body
+    //     )}
+    // </>
   );
 };
 
