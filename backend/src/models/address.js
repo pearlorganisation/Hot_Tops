68747c5d6c303094818486698099a5112d2020c0
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-08-26 12:40:28 +0530

    Adddress module completed

--- backend/src/models/address.js
+++ backend/src/models/address.js
@@ -5,10 +5,6 @@ const addressSchema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         required:[true,"User Id is required! "]
     },
-name:{
-    type:String,
-    // required:[true,'Name is required!']
-},
 address:{
     type:String,
     required:[true,"Address is Required!"]
