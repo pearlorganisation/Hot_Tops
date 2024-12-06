3545b1429747616a8b1fa2266ebb85aef5398ef5
Avnish <avnish@pearlorganisation.com>
2024-12-06 16:32:25 +0530

    avdvs

--- backend/src/models/foodModels/foodCustomization/pizzaCustomization/size.js
+++ backend/src/models/foodModels/foodCustomization/pizzaCustomization/size.js
@@ -3,10 +3,13 @@ import mongoose from "mongoose";
 // ----------------------------------------------------------------------------------------------------------
 
 const sizeCustomizationSchema = new mongoose.Schema({
- 
       name: {
         type: String,
         required: [true, "Size Name is a required field"],
+      },
+      basePrice: {
+        type: Number,
+        required: [true, "Deals Base Price is a required field"],
       }
     }
 
